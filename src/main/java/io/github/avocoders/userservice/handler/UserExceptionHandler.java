86f8ab51e7809ad8eb96ffac2c33eb4328079431
Veronika Belova <belovaveronika14@gmail.com>
2026-08-18 13:29:24 +0300

    rewrite class UserExceptionHandler

---
+++ src/main/java/io/github/avocoders/userservice/handler/UserExceptionHandler.java
@@ -0,0 +1,13 @@
+package io.github.avocoders.userservice.handler;
+
+import io.github.avocoders.userservice.exception.UserPersistenceException;
+import io.github.avocoders.userservice.exception.UserValidationException;
+
+public class UserExceptionHandler {
+    public  void handle(UserValidationException exception){
+       System.out.println("Validation error: " + exception.getMessage());
+    }
+    public void handle(UserPersistenceException exception){
+        System.out.println("Database error: " + exception.getMessage());
+    }
+}
