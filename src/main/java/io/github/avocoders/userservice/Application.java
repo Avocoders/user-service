ba5a2e3d675ef59903a4eaefa9bc14b84391b227
Veronika Belova <belovaveronika14@gmail.com>
2026-08-18 13:11:49 +0300

    rewrite class Application

--- src/main/java/io/github/avocoders/userservice/Application.java
+++ src/main/java/io/github/avocoders/userservice/Application.java
@@ -1,8 +1,12 @@
 package io.github.avocoders.userservice;
 
-import io.github.avocoders.userservice.entity.User;
+import io.github.avocoders.userservice.dtos.UserDto;
+import io.github.avocoders.userservice.mappers.UserMapper;
 import io.github.avocoders.userservice.repository.HibernateUserRepository;
 import io.github.avocoders.userservice.repository.UserRepository;
+import io.github.avocoders.userservice.service.UserService;
+import io.github.avocoders.userservice.service.UserServiceImpl;
+import io.github.avocoders.userservice.validators.UserValidator;
 import org.hibernate.SessionFactory;
 import io.github.avocoders.userservice.config.HibernateUtil;
 
@@ -13,7 +17,10 @@ public class Application {
     public static void main(String[] args){
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         UserRepository userRepository = new HibernateUserRepository(sessionFactory);
-        User user = new User("Name", "name5@email.ru", 30);
+        UserMapper userMapper = new UserMapper();
+        UserValidator userValidator = new UserValidator();
+        UserService userService = new UserServiceImpl(userRepository, userMapper, userValidator);
+
         try{
             if (sessionFactory.isOpen()) {
                 System.out.println("Connected");
@@ -21,29 +28,29 @@ public class Application {
             else {
                 System.out.println("Disconnected");
             }
-            User savedUser = userRepository.save(user);
-            System.out.println(savedUser);
-
-            Optional<User> findUser = userRepository.findById(savedUser.getId());
-            findUser.ifPresentOrElse(System.out::println,
-                        () -> System.out.println("User not found"));
-
-            List<User> findAll = userRepository.findAll();
-            findAll.forEach(System.out::println);
-
-            savedUser.setName("Name2");
-            savedUser.setAge(32);
-            User updatedUser = userRepository.update(savedUser);
-            System.out.println("Updated: " + updatedUser);
-
-            Optional<User> findUser2 = userRepository.findById(updatedUser.getId());
-            findUser2.ifPresentOrElse(System.out::println,
-                    () -> System.out.println("User not found"));
-
-            boolean deleted = userRepository.deleteById(updatedUser.getId());
+            UserDto createdUser = userService.createUser("Name", "name6@mail.ru", 30);
+            System.out.println("Created: " + createdUser);
+            Optional<UserDto> foundUser = userService.getUserById(createdUser.getId());
+            foundUser.ifPresentOrElse(
+                    value -> System.out.println("Found: " + value),
+                    () -> System.out.println("User not found")
+            );
+            List<UserDto> users = userService.getAllUsers();
+            System.out.println("All users:");
+            users.forEach(System.out::println);
+            Optional<UserDto> updatedUser = userService.updateUser(
+                    createdUser.getId(),
+                    "Update name",
+                    "updated6@mail.ru",
+                    32
+            );
+            updatedUser.ifPresentOrElse(
+                    value -> System.out.println("Updated: " + value),
+                    () -> System.out.println("User not found")
+            );
+            boolean deleted = userService.deleteUser(createdUser.getId());
             System.out.println("Deleted: " + deleted);
-
-            userRepository.findById(updatedUser.getId())
+            userService.getUserById(createdUser.getId())
                     .ifPresentOrElse(
                             System.out::println,
                             () -> System.out.println("User was deleted")
