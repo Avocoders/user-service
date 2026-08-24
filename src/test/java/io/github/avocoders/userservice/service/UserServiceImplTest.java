c399c7b50c53af3d3e6df4207b803b1d0009fa7d
Veronika Belova <belovaveronika14@gmail.com>
2026-08-24 20:51:00 +0300

    test for createUser

---
+++ src/test/java/io/github/avocoders/userservice/service/UserServiceImplTest.java
@@ -0,0 +1,46 @@
+package io.github.avocoders.userservice.service;
+
+import io.github.avocoders.userservice.dtos.UserDto;
+import io.github.avocoders.userservice.entity.User;
+import io.github.avocoders.userservice.mappers.UserMapper;
+import io.github.avocoders.userservice.repository.UserRepository;
+import io.github.avocoders.userservice.validators.UserValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserMapper userMapper;
+    @Mock
+    private UserValidator userValidator;
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    void createUser_shouldReturnDto_whenInputIsValid() {
+
+        String name = "Veronika";
+        String email = "veronika@ya.ru";
+        Integer age = 32;
+        User savedUser = new User(name, email, age);
+        UserDto expectedDto = new UserDto(1L, name, email);
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+        when(userMapper.toDto(savedUser)).thenReturn(expectedDto);
+
+        UserDto actualDto = userService.createUser(name, email, age);
+
+        assertSame(expectedDto, actualDto);
+
+    }
+}
