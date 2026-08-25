56edc69a0f77ac29724618beccac72aaf5a46596
Veronika Belova <belovaveronika14@gmail.com>
2026-08-25 17:48:31 +0300

    add test for UserValidator Name and Email

--- src/test/java/io/github/avocoders/userservice/validators/UserValidatorTest.java
+++ src/test/java/io/github/avocoders/userservice/validators/UserValidatorTest.java
@@ -33,7 +33,61 @@ class UserValidatorTest {
     @Test
     void validateId_shouldThrowException_whenIdIsNull() {
         Long id = null;
-
         assertThrows( UserValidationException.class, () -> userValidator.validateId(id));
     }
+
+    @Test
+    void validateName_whenNameIsValid() {
+        String name = "Veronika";
+        assertDoesNotThrow(() -> userValidator.validateName(name));
+    }
+
+    @Test
+    void validateName_shouldThrowException_whenNameIsNull() {
+        String name = null;
+        assertThrows( UserValidationException.class, () -> userValidator.validateName(name));
+    }
+
+    @Test
+    void validateName_shouldThrowException_whenNameIsBlank() {
+        String name = " ";
+        assertThrows( UserValidationException.class, () -> userValidator.validateName(name));
+    }
+
+    @Test
+    void validateName_shouldThrowException_whenNameLengthMore100() {
+        String name = "f".repeat(101);
+        assertThrows(UserValidationException.class, () -> userValidator.validateName(name));
+    }
+
+    @Test
+    void validateEmail_whenEmailIsValid() {
+        String email = "aleksey@ya.com";
+        assertDoesNotThrow( () -> userValidator.validateEmail(email));
+    }
+
+    @Test
+    void validateEmail_shouldThrowException_whenEmailIsNull() {
+        String email = null;
+        assertThrows( UserValidationException.class, () -> userValidator.validateEmail(email));
+    }
+
+    @Test
+    void validateEmail_shouldThrowException_whenEmailIsBlank() {
+        String email = "  ";
+        assertThrows( UserValidationException.class, () -> userValidator.validateEmail(email));
+    }
+
+    @Test
+    void validateEmail_shouldThrowException_whenEmailLengthMore255() {
+        String email = "f".repeat(257) + "@ya.ru";
+        assertThrows( UserValidationException.class, () -> userValidator.validateEmail(email));
+    }
+
+    @Test
+    void validateEmail_shouldThrowException_whenEmailIsInvalid() {
+        String email = "k.jnfgiusd@dfbd";
+        assertThrows( UserValidationException.class, () -> userValidator.validateEmail(email));
+    }
+
 }
